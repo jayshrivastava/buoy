4417a812f86872ef1c246b1a0cc96a75df7fb2b7
Jayant Shrivastava <jshrivastava03@gmail.com>
2020-03-28 12:31:09 -0400

    Updated logger to use html tables

--- logger.go
+++ logger.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"github.com/jayshrivastava/buoy/node"
 	"os"
-	"text/tabwriter"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -14,31 +15,57 @@ func formatTime(t time.Time) string {
 
 type logger struct {
 	totalNodes int32
-	counter    int32
 	stream     chan string
+	terminated chan bool
 }
 
 func CreateLogger(totalNodes int32) node.Logger {
-	logger := logger{totalNodes: totalNodes, counter: 0, stream: make(chan string)}
-	go logger.listen()
-	message := "\t"
-	for i := int32(0); i < logger.totalNodes; i++ {
-		message = message + fmt.Sprintf("%d\t", i)
+	logger := logger{
+		totalNodes: totalNodes,
+		stream:     make(chan string),
+		terminated: make(chan bool, 1),
 	}
+	go logger.listen()
+	go logger.terminationHandler()
+	logger.stream <- fmt.Sprintf("<style>%s</style", CSS)
+	logger.stream <- "<html>"
+	logger.stream <- "<table>"
+
+	message := "<tr><td></td>"
+	for i := int32(0); i < logger.totalNodes; i++ {
+		message = message + fmt.Sprintf("<td>%d</td>", i)
+	}
+	message = message + "</tr>"
+
 	logger.stream <- fmt.Sprintf(message)
 
 	return &logger
 }
 
+func (l *logger) terminationHandler() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT)
+
+	_ = <-sigs
+
+	l.stream <- TERMINATION_STR
+
+	_ = <-l.terminated
+
+	syscall.Kill(os.Getpid(), syscall.SIGTERM)
+}
+
 func (l *logger) Log(id int32, message string) {
 
-	line := fmt.Sprintf("%s\t", formatTime(time.Now()))
+	line := fmt.Sprintf("<tr><td>%s</td>", formatTime(time.Now()))
 	for i := int32(0); i < l.totalNodes; i++ {
 		if i == id {
-			line = line + fmt.Sprintf("%s", message)
+			line = line + fmt.Sprintf("<td>%s</td>", message)
+		} else {
+			line = line + "<td></td>"
 		}
-		line = line + "\t"
 	}
+	line = line + "</tr>"
 
 	l.stream <- line
 }
@@ -47,23 +74,34 @@ func (l *logger) listen() {
 	f, _ := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
 
-	w := tabwriter.NewWriter(f, 0, 0, 1, ' ', 0)
-
 	for {
 		message := <-l.stream
 
-		fmt.Fprintln(w, message)
-		l.counter += 1
-		if l.counter == BUFFER_COUNT {
-			w.Flush()
-			l.counter = 0
+		if message == TERMINATION_STR {
+			fmt.Fprintln(f, "</table>")
+			fmt.Fprintln(f, "</html>")
+			l.terminated <- true
+			return
 		}
+
+		fmt.Fprintln(f, message)
 
 	}
 }
 
 /* CONSTANTS */
 const (
-	LOG_FILE     = "log.txt"
-	BUFFER_COUNT = 100
+	LOG_FILE        = "log.html"
+	TERMINATION_STR = "TERMINATE"
+	CSS             = `
+	table{
+        border-collapse:collapse;
+        border:1px solid #000000;
+    }
+    table td{
+		border:1px solid #000000;
+		max-width: 150px;
+		padding: 5px;
+    }
+	`
 )
